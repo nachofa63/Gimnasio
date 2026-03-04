9ed51bb17bf420d2972d23e082f0fd2a25540fe7
nachofa63 <nachofa63@educastur.es>
2026-03-04 20:04:12 +0100

    Cambio nombre clase y añadido nombre

--- src/codigo/AppPrincipal.java
+++ src/codigo/AppPrincipal.java
@@ -1,10 +1,12 @@
 package codigo;
 
-public class App {
+public class AppPrincipal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Este archivo lanzará la aplicación");
+		
+		System.out.println("Nacho Fernández Álvarez");
 	}
 
 }
